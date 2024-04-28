3b35e6ba9bf85ba165abb20246d6d0ef9eaef69b
RakibulB <rakibulbhuiyan1973@gmail.com>
2024-04-28 04:19:34 +0100

    minor

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,19 +5,19 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-      <div className="h-full fixed flex flex-col justify-end items-center">
-          <div className="flex flex-col gap-y-10 mb-10">
-            <Link href="https://github.com/RakibulBh">
-              <FiGithub className="transition-colors duration-300 ease-in-out hover:text-green-500 cursor-pointer text-white h-6 w-6"/>
-            </Link>
-            <Link href="https://linkedin.com/in/RakibulB">
-              <FaLinkedinIn className="transition-colors duration-300 ease-in-out hover:text-green-500 cursor-pointer text-white h-6 w-6" />
-            </Link>
-            <Link href="https://leetcode.com/RakibulB">
-              <SiLeetcode className="transition-colors duration-300 ease-in-out hover:text-green-500 cursor-pointer text-white h-6 w-6" />
-            </Link>
-          </div>
-          <div className="h-[8rem] w-1 rounded-full bg-white"></div>
+    <div className="h-full fixed flex flex-col justify-end items-center">
+      <div className="flex flex-col gap-y-10 mb-10">
+        <Link href="https://github.com/RakibulBh">
+          <FiGithub className="transition-transform duration-300 ease-in-out transform hover:-translate-y-1 cursor-pointer text-white hover:text-green-500 h-6 w-6" />
+        </Link>
+        <Link href="https://linkedin.com/in/RakibulB">
+          <FaLinkedinIn className="transition-transform duration-300 ease-in-out transform hover:-translate-y-1 cursor-pointer text-white hover:text-green-500 h-6 w-6" />
+        </Link>
+        <Link href="https://leetcode.com/RakibulB">
+          <SiLeetcode className="transition-transform duration-300 ease-in-out transform hover:-translate-y-1 cursor-pointer text-white hover:text-green-500 h-6 w-6" />
+        </Link>
       </div>
-  )
+      <div className="h-[8rem] w-1 rounded-full bg-white"></div>
+    </div>
+  );
 }
