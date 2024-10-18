1e18b7c9441dc64b75eef9fe2f1c1068cbe1b125
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-10-18 22:44:30 +0100

    restart

--- components/SectionTitle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SectionTitle({title, number}) {
-  return (
-    <h1 className="text-xl md:text-xl"><span className="text-green-500">0{number}. </span>{title}</h1>
-  )
-}
--- components/Sections/Skills/Skill.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Skill({skill}) {
-  return (
-    <span className='text-xs md:text-sm text-white bg-[#334155] px-2 py-1 rounded-md mx-1 mb-1'>
-      {skill}
-    </span>
-  )
-}
