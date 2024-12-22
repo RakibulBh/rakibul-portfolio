1a25418ad9b7c4eea6c3871dbe96e61e4efcebef
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-12-22 21:12:03 +0000

    fix: eslint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { Poppins } from "next/font/google";
-import { useState } from "react";
 
 const poppins = Poppins({
   subsets: ["latin"],
