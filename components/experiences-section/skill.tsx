6e462ae71dc80e9833435e143844c311a425cb1c
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-02-23 18:35:51 +0000

    delete everything

--- components/experiences-section/skill.tsx
+++
@@ -1,7 +0,0 @@
-export const Skill = ({ skill }: { skill: string }) => {
-  return (
-    <div className="w-12 flex items-center justify-center p-1 bg-white bg-opacity-5 border-white border-[1px] border-opacity-15">
-      <p className="text-[10px] text-[#00B8D9] truncate">{skill}</p>
-    </div>
-  );
-};
